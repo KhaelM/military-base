652842acd8ff7adf798e150a92963d5aca3e7227
KhaelM <mikerandria@gmail.com>
2019-06-07 01:05:55 +0300

    descriptionEtat added

--- src/service/ServiceMission.java
+++ src/service/ServiceMission.java
@@ -10,6 +10,21 @@ public class ServiceMission {
 
     private DAOMission daoMission;
 
+    public static String descriptionEtat(Integer etat) throws Exception {
+        switch (etat.intValue()) {
+            case Mission.ETAT_DISPO:
+                return "disponible";
+            case Mission.ETAT_EN_COURS:
+                return "en cours";        
+            case Mission.ETAT_SUCCES:
+                return "succès";
+            case Mission.ETAT_ECHEC:
+                return "échec";
+            default:
+                throw new Exception("Etat inexistant.");
+        }
+    }
+
     public ServiceMission(DAOMission daoMission) {
         this.daoMission = daoMission;
     }
@@ -21,4 +36,5 @@ public class ServiceMission {
     public Mission trouverMission(Long id) {
         return daoMission.read(id);
     }
+
 }
